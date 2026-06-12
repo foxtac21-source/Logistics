01fe2e8410a296ea5615cb72252280e5522e80de
foxtac21-source <fox_tac21@mail.ru>
2026-06-12 18:13:20 +0400

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,18 @@
+<?php
+$host = 'localhost';
+$dbname = 'TransportOrdersDB';
+$username = 'root';
+$password = '';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+} catch(PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+?>
